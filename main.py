f11a8861b1ac0d26205f25cb1af3a077b1fd1d36
malo-gv <148189697+malo-gv@users.noreply.github.com>
2023-11-14 10:02:07 +0100

    add print("coucou") (#1)

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from functions import *
 
+print("coucou")
+
 directory = 'C:\\Users\\malog\\OneDrive\\Documents\\PROJET CHATBOT\\speeches-20231110.zip'
 files_names = (list_of_files(directory, "txt"))
 print(list(files_names))
+
