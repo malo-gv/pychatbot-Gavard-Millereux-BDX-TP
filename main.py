6a64ebe8b274ab02f36b4e1817ef4e78567dcb07
malo-gv <148189697+malo-gv@users.noreply.github.com>
2023-11-14 11:38:31 +0100

    ajout des fichiers discours (#2)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from functions import *
 
 print("coucou")
 
-directory = 'C:\\Users\\malog\\OneDrive\\Documents\\PROJET CHATBOT\\speeches-20231110.zip'
+directory = 'ressources/speeches-20231110/'
 files_names = (list_of_files(directory, "txt"))
 print(list(files_names))
 
