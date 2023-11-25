95036934ed7cf78b522a9998381d926db07a5c1a
malo-gv <malogavard@gmail.com>
2023-11-25 14:20:14 +0100

    FINI PARTIE 1

--- functions.py
+++ functions.py
@@ -100,3 +100,91 @@ def nettoyer_texte(nom_fichier):
         fichier_destination.write(contenu_final)
     print("On a enlevé le reste")
 
+
+def calculer_tf(nom_fichier):
+    chemin_fichier = "./ressources/cleaned/" + nom_fichier
+    with open(chemin_fichier, 'r', encoding='utf8') as fichier:
+        texte = fichier.read()
+
+    mots = texte.split()
+    frequence = {}
+
+    for mot in mots:
+        frequence[mot] = frequence.get(mot, 0) + 1
+    return frequence
+
+
+# Fonction pour calculer le score IDF dans l'ensemble du corpus en utilisant TF-IDF
+def calculer_idf(directory):
+    nombre_documents = 0
+    idf = {}
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):  # Assurez-vous que les fichiers ont l'extension .txt
+            nombre_documents += 1
+            chemin_fichier = os.path.join(directory, filename)
+            with open(chemin_fichier, 'r', encoding='utf8') as fichier:
+                contenu = fichier.read()
+            mots_uniques = set(contenu.split())
+
+            for mot in mots_uniques:
+                idf[mot] = idf.get(mot, 0) + 1
+
+    for mot, nombre_apparitions in idf.items():
+        idf[mot] = math.log(nombre_documents / (1 + nombre_apparitions))  # On ajoute 1 pour ne pas diviser par 0
+
+    return idf
+
+
+def generer_matrice(directory):
+    mots_uniques = set()
+
+    # Construire un ensemble de tous les mots uniques dans le corpus
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            chemin_fichier = os.path.join(directory, filename)
+            with open(chemin_fichier, 'r', encoding='utf8') as fichier:
+                contenu = fichier.read()
+            mots_uniques.update(set(contenu.split()))
+
+    idf = calculer_idf(directory)
+
+    matrice = []
+
+    # Pour chaque mot unique, construire son vecteur TF-IDF
+    for mot in mots_uniques:
+        vecteur = []
+
+        # Pour chaque document, calculer le score TF-IDF du mot
+        for filename in os.listdir(directory):
+            if filename.endswith(".txt"):
+                nom_fichier = os.path.join(filename)
+
+                # Calculer le score TF pour le mot dans le document
+                tf = calculer_tf(nom_fichier).get(mot, 0)
+
+                # Calculer le score TF-IDF en multipliant le score TF par le score IDF
+                tfidf = tf * idf[mot]
+
+                # Ajouter le score TF-IDF au vecteur
+                vecteur.append(tfidf)
+
+        # Ajouter le vecteur TF-IDF à la matrice
+        matrice.append(vecteur)
+
+    return matrice
+
+
+# Fonction pour calculer la transposée d'une matrice
+def calculer_transposee(matrice):
+    # On échange les lignes et les colonnes
+    return [[matrice[j][i] for j in range(len(matrice))] for i in range(len(matrice[0]))]
+
+def mots_non_importants(matrice):
+    mots_non_importants = []
+
+    for i, mot in enumerate(matrice):
+        if all(score == 0 for score in mot):
+            mots_non_importants.append(mot)
+
+        return mots_non_importants
