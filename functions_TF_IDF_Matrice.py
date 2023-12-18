823d0c807b996649b6e91a5c199f8b744e9778e0
malo-gv <malogavard@gmail.com>
2023-12-18 21:05:22 +0100

    réorganisation

--- functions_TF_IDF_Matrice.py
+++ functions_TF_IDF_Matrice.py
@@ -142,6 +142,30 @@ def document_pertinent(tfidf_corpus, tfidf_question):
     return most_similar_document_name
 
 
+def tokeniser_question(question):
+    contenu_sans_ponctuation = ''.join(
+        caractere if caractere not in string.punctuation else ' ' for caractere in question)
+    contenu_traite = []
+    mots = contenu_sans_ponctuation.split()
+    for mot in mots:
+        if '-' in mot:
+            sous_mots = mot.split('-')
+            contenu_traite.extend(sous_mots)
+        elif "'" in mot:
+            sous_mots = mot.split("'")
+            contenu_traite.extend(sous_mots)
+        else:
+            contenu_traite.append(mot)
+    contenu_final = ' '.join(contenu_traite)
+    mots = contenu_final.split()
+    return mots
+
+def rechercher_mot_quest(liste_mots, mots_corpus):
+    liste_inter = []
+    for mot in liste_mots:
+        if mot in mots_corpus :
+            liste_inter.append(mot)
+    return liste_inter
 
 
 def generer_reponse(question, directory):
