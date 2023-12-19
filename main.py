b726f07a2da78f6663093fa7d351cbe734da352c
malo-gv <148189697+malo-gv@users.noreply.github.com>
2023-12-19 10:07:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,61 +28,67 @@ calculer_idf(directory)
 
 matrice = generer_matrice(directory)
 
-"""print("Menu :")
+print("Menu :")
+print("1. Accéder aux fonctionnalités de la partie 1.")
+print("2. Accéder aux fonctionnalités ChatBot.")
 print("0. Annuler.")
-print("1. Afficher la liste des mots les moins importants dans le corpus de documents.")
-print("2. Afficher le(s) mot(s) ayant le score TD-IDF le plus élevé.")
-print("3. Trouver le mot le plus répété par Chirac.")
-print("4. Identifier le président parlant de la ""Nation"" et celui en parlant le plus.")
-print("5. Indiquer le premier président à parler du climat et/ou de l’écologie.")
-print("6. Trouver les mots communs mentionnés par tous les présidents.")
-print("7. Posez VOTRE propre question.")
-print()
-print("Si vous obtenez le message ""PermissionError: [Errno 13] Permission denied"" veuillez relancer le programme.")
 
-choix = 0
-while choix <= 0 or choix > 7:
-    choix = int(input("Entrez votre choix (1-7) : "))
-    if choix == 1:
-        print("Les mots les moins importants sont : ", mots_non_importants(directory))
-        break
-    if choix == 2:
-        print("Le mot le plus important est : ", mot_plus_important(directory))
-        break
-    if choix == 3:
-        print("Le mot le plus répété par J.Chirac est : ", mots_plus_repetes_par_chirac(directory))
-        break
-    if choix == 4:
-        president_nation(directory)
-        break
-    if choix == 5:
-        print("Le premier président à parler de l'écologie est : ", president_ecologie(directory))
-        break
-    if choix == 6:
-        print("Les mots évoquées par tous les présidents sont : ",mots_evoques(directory))
-        break
-    if choix == 0:
-        print("Fin du programme. Au revoir !")
-        break
-    if choix == 7:
-        question = input("Veuillez entrer votre message (SANS ACCENTS NI APOSTROPHES :")
-        
-    else :
-        print("! ERREUR !")
-        print("Veuillez entrer un chiffre compris entre 1 et 7 SVP !")"""
+choix1 = -1
+while choix1 <= 0 or choix1 > 2:
+    choix1 = int(input("Entrez votre choix (0-2) : "))
+if choix1 == 1:
+    print("1. Afficher la liste des mots les moins importants dans le corpus de documents.")
+    print("2. Afficher le(s) mot(s) ayant le score TD-IDF le plus élevé.")
+    print("3. Trouver le mot le plus répété par Chirac.")
+    print("4. Identifier le président parlant de la ""Nation"" et celui en parlant le plus.")
+    print("5. Indiquer le premier président à parler du climat et/ou de l’écologie.")
+    print("6. Trouver les mots communs mentionnés par tous les présidents.")
+    print()
+    print("Si vous obtenez le message ""PermissionError: [Errno 13] Permission denied"" veuillez relancer le programme.")
+
+    choix = 0
+    while choix <= 0 or choix > 7:
+        choix = int(input("Entrez votre choix (1-7) : "))
+        if choix == 1:
+            print("Les mots les moins importants sont : ", mots_non_importants(directory))
+            break
+        if choix == 2:
+            print("Le mot le plus important est : ", mot_plus_important(directory))
+            break
+        if choix == 3:
+            print("Le mot le plus répété par J.Chirac est : ", mots_plus_repetes_par_chirac(directory))
+            break
+        if choix == 4:
+            president_nation(directory)
+            break
+        if choix == 5:
+            print("Le premier président à parler de l'écologie est : ", president_ecologie(directory))
+            break
+        if choix == 6:
+            print("Les mots évoquées par tous les présidents sont : ",mots_evoques(directory))
+            break
+        if choix == 0:
+            print("Fin du programme. Au revoir !")
+            break
+
+        else :
+            print("! ERREUR !")
+            print("Veuillez entrer un chiffre compris entre 0 et 6 SVP !")
+
+if choix1 == 2:
+    print("Si vous obtenez le message ""PermissionError: [Errno 13] Permission denied"" veuillez relancer le programme.")
+    print("Posez votre question au ChatBot :")
+    question = input()
 
 
-question = "Peux-tu me dire comment une nation peut-elle prendre soin de la nation , il est important de prendre en compte la nation ?"
-liste_mots = tokeniser_question(question)
-print("1",liste_mots)
+    liste_mots = tokeniser_question(question)
 
+    mots_corpus = recuperer_tous_les_mots(directory)
 
-mots_corpus = recuperer_tous_les_mots(directory)
+    rechercher_mot_quest(liste_mots, mots_corpus)
 
-print("2",rechercher_mot_quest(liste_mots, mots_corpus))
+    tf_quest = calculer_vecteur_tf_idf_question(question, directory)
 
-tf_quest = calculer_vecteur_tf_idf_question(question, directory)
+    doc = document_pertinent(matrice, tf_quest)
 
-doc = document_pertinent(matrice, tf_quest)
-
-print(generer_reponse(question, 'ressources/speeches-20231110/', doc, tf_quest))
+    print(generer_reponse(question, 'ressources/speeches-20231110/', doc, tf_quest))
